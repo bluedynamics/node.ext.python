22774c01a8352782193ce94138c0a472bd6af68d
Christoph Scheid <c@shri.de>
2013-01-14 12:32:58 +0100

    pep8 / whitespace

--- src/node/ext/python/interfaces.py
+++ src/node/ext/python/interfaces.py
@@ -26,7 +26,7 @@ class Incomplete(Exception):
 class IPythonNode(INode, ICallable):
     """Python node.
     """
-    
+
     buffer = Attribute(u"The existing file line buffer als list. Read-only.")
     astnode = Attribute(u"The python node refering ast node.")
     startlineno = Attribute(u"Start line number of python Node. Read-only.")
@@ -37,31 +37,31 @@ class IPythonNode(INode, ICallable):
     postlf = Attribute(u"Number of Newlines to render on __call__ after Node.")
     # XXX propably move ``nodelevel`` to Node under name level
     nodelevel = Attribute(u"The current node level.")
-    
+
     def docstrings():
         """Return docstrings.
         """
-    
+
     def blocks():
         """Return blocks.
         """
-    
+
     def imports():
         """Return blocks.
         """
-        
+
     def protectedsections(name=None):
         """Return protected sections. If name is not None, filter by name.
         """
-    
+
     def classes(name=None):
         """Return classes. If name is not None, filter by name.
         """
-    
+
     def functions(name=None):
         """Return functions. If name is not None, filter by name.
         """
-    
+
     def attributes(name=None):
         """Return attributes. If name is not None, filter by name.
         """
@@ -70,7 +70,7 @@ class IPythonNode(INode, ICallable):
 class IModule(IFile, IPythonNode):
     """Python module.
     """
-    
+
     modulename = Attribute(u"The name of the module.")
     encoding = Attribute(u"The file encoding. Defaults to utf-8")
     bufoffset = Attribute(u"Number of lines stripped at the top of a "
@@ -80,7 +80,7 @@ class IModule(IFile, IPythonNode):
 class IBlock(IPythonNode):
     """Python code block.
     """
-    
+
     lines = Attribute(u"List of code lines.")
     text = Attribute(u"block contents as text.")
 
@@ -88,7 +88,7 @@ class IBlock(IPythonNode):
 class IProtectedSection(IPythonNode):
     """Protected section.
     """
-    
+
     lines = Attribute(u"List of code lines.")
     text = Attribute(u"block contents as text.")
 
@@ -96,7 +96,7 @@ class IProtectedSection(IPythonNode):
 class IDocstring(IPythonNode):
     """Docstring.
     """
-    
+
     lines = Attribute(u"The doc string contents.")
     text = Attribute(u"block contents as text.")
 
@@ -104,7 +104,7 @@ class IDocstring(IPythonNode):
 class IImport(IPythonNode):
     """Import line.
     """
-    
+
     fromimport = Attribute("The module name from import or None")
     names = Attribute(u"List of tuples containing (importname, asname)")
 
@@ -112,7 +112,7 @@ class IImport(IPythonNode):
 class IAttribute(IPythonNode):
     """Attribute.
     """
-    
+
     targets = Attribute(u"The targets of the attribute.")
     value = Attribute(u"The attribute value.")
 
@@ -120,19 +120,19 @@ class IAttribute(IPythonNode):
 class ICallableArguments(Interface):
     """Arguments of a callable, usually a decosrator or a function.
     """
-    
+
     args = Attribute(u"The callable arguments")
     kwargs = Attribute(u"The callable kwarguments")
     s_args = Attribute(u"The callable arguments as string")
     s_kwargs = Attribute(u"The callable kwarguments as string")
-    
+
     def extract_arguments():
         """Return 2-tuple with args and kwargs.
-        
+
         The arguments are computed by looking up the string value for s_args
         and s_kwargs, as fallback args and kwargs are used.
         """
-    
+
     def arguments_equal(other):
         """Check whether other callable arguments are equal to self.
         """
@@ -141,23 +141,25 @@ class ICallableArguments(Interface):
 class IDecorator(IPythonNode, ICallableArguments):
     """Decorator.
     """
-    
+
     decoratorname = Attribute(u"The name of the decorator.")
-    
+
     def equals(other):
         """Check whether other decorator is equal to self.
         """
 
+
 class IDecorable(Interface):
     '''decorable nodes (currently Function, Class'''
+
 
 class IFunction(IPythonNode, ICallableArguments):
     """Python function.
     """
-    
+
     functionname = Attribute(u"Name of the function")
     defendlineno = Attribute(u"End line number of function def. Read-only.")
-    
+
     def decorators(name=None):
         """Return decorators. If name is not None, filter by name.
         """
@@ -166,11 +168,11 @@ class IFunction(IPythonNode, ICallableArguments):
 class IClass(IPythonNode):
     """Python class.
     """
-    
+
     classname = Attribute(u"Name of the class.")
     bases = Attribute(u"List of base classes for this class")
     defendlineno = Attribute(u"End line number of class def. Read-only.")
-    
+
     def decorators(name=None):
         """Return decorators. If name is not None, filter by name.
         """
