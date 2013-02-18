b3ee9a481f3150dfa5cc88f5ce4ad7f76d0d5983
Christoph Scheid <c@shri.de>
2013-02-18 19:09:34 +0100

    unittest stub. go, gogo, go

---
+++ src/node/ext/python/tests/test_goparser.py
@@ -0,0 +1,28 @@
+import unittest
+
+class TestGoParser(unittest.TestCase):
+    """
+    tests for the goparser.GoParser
+    """
+
+    def setUp(self):
+        """
+        set up stuff for testing
+        """
+        pass
+
+    def test_foo(self):
+        """
+        test some aspect of foo
+        """
+        self.assertTrue(1 == True)
+
+    def tearDown(self):
+        """
+        clean up after tests
+        """
+        pass
+
+
+
+
